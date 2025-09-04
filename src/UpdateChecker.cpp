54345ba8e898e24b72bf7c43707708bb76b6db22
Kaito Udagawa <umireon@gmail.com>
2025-09-05 05:08:55 +0900

    Add UpdateChecker class with tests and integrate into build
    Introduces the UpdateChecker class for checking the latest release from GitHub using Qt6::Network. Updates CMakeLists.txt to include Qt6::Network and the new source file. Adds a corresponding unit test (UpdateChecker_test.cpp) and updates test configuration to build and run the new test. Also enables verbose output for ctest in the CI workflow.

---
+++ src/UpdateChecker.cpp
@@ -0,0 +1,49 @@
+/*
+obs-showdraw
+Copyright (C) 2025 Kaito Udagawa umireon@kaito.tokyo
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program. If not, see <https://www.gnu.org/licenses/>
+*/
+
+#include "UpdateChecker.hpp"
+
+#include <QNetworkReply>
+#include <QDebug>
+
+UpdateChecker::UpdateChecker(QObject *parent) : QObject(parent)
+{
+    manager = new QNetworkAccessManager();
+}
+
+void UpdateChecker::fetch(void)
+{
+    QNetworkRequest request(QUrl("https://api.github.com/repos/kaito-tokyo/obs-showdraw/releases/latest"));
+    QNetworkReply *reply = manager->get(request);
+    connect(reply, &QNetworkReply::finished, this, [this, reply]() {
+        if (reply->error() != QNetworkReply::NoError) {
+            // Handle error
+            reply->deleteLater();
+            return;
+        }
+
+        QByteArray responseData = reply->readAll();
+        qDebug() << "Response Data:" << responseData;
+
+        reply->deleteLater();
+    });
+}
+
+void UpdateChecker::isUpdateAvailable(void) const noexcept
+{
+}
---
+++ src/UpdateChecker.hpp
@@ -0,0 +1,33 @@
+/*
+obs-showdraw
+Copyright (C) 2025 Kaito Udagawa umireon@kaito.tokyo
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program. If not, see <https://www.gnu.org/licenses/>
+*/
+
+#pragma once
+
+#include <QNetworkAccessManager>
+
+class UpdateChecker : public QObject {
+    Q_OBJECT
+
+public:
+    explicit UpdateChecker(QObject *parent = nullptr);
+    void fetch(void);
+    void isUpdateAvailable(void) const noexcept;
+
+private:
+    QNetworkAccessManager *manager;
+};
