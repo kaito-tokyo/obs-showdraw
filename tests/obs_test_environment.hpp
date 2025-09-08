9f6c384a2cfc4b78086df0ba08ff23561cc8428d
Kaito Udagawa <umireon@gmail.com>
2025-09-09 05:00:47 +0900

    Update obs_test_environment.hpp

--- tests/obs_test_environment.hpp
+++ tests/obs_test_environment.hpp
@@ -56,13 +56,13 @@ public:
 	void SetUp() override
 	{
 #if defined(_WIN32)
-		const char* data_path_env = "%ProgramFiles%\\obs-studio\\data";
+		const char *data_path_env = "%ProgramFiles%\\obs-studio\\data";
 		char data_path[MAX_PATH];
 		ExpandEnvironmentStringsA(data_path_env, data_path, MAX_PATH);
 #elif defined(__APPLE__)
-		const char* data_path = nullptr;
+		const char *data_path = nullptr;
 #else
-		const char* data_path = nullptr;
+		const char *data_path = nullptr;
 #endif
 		if (!obs_startup("en-US", data_path, nullptr)) {
 			FAIL() << "OBS startup failed.";
