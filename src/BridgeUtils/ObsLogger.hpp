e4c7fef78ea7b55456ea68e32d8bc2ee8f963e62
Kaito Udagawa <umireon@gmail.com>
2025-09-19 07:52:51 +0900

    Update ObsLogger.hpp

--- src/BridgeUtils/ObsLogger.hpp
+++ src/BridgeUtils/ObsLogger.hpp
@@ -18,6 +18,10 @@ with this program. If not, see <https://www.gnu.org/licenses/>
 
 #pragma once
 
+#include <mutex>
+#include <sstream>
+#include <string_view>
+
 #include <backward.hpp>
 
 #include <util/base.h>
@@ -30,55 +34,67 @@ namespace BridgeUtils {
 
 class ObsLogger final : public ILogger {
 public:
-	ObsLogger(const std::string &_prefix) : prefix(_prefix) {}
+    ObsLogger(const std::string &_prefix) : prefix(_prefix) {}
 
 protected:
-	void log(LogLevel level, std::string_view message) const noexcept override
-	{
-		int blogLevel;
-		switch (level) {
-		case LogLevel::Debug:
-			blogLevel = LOG_DEBUG;
-			break;
-		case LogLevel::Info:
-			blogLevel = LOG_INFO;
-			break;
-		case LogLevel::Warn:
-			blogLevel = LOG_WARNING;
-			break;
-		case LogLevel::Error:
-			blogLevel = LOG_ERROR;
-			break;
-		default:
-			fprintf(stderr, "[LOGGER FATAL] Unknown log level: %d\n", static_cast<int>(level));
-			return;
-		}
+    void log(LogLevel level, std::string_view message) const noexcept override
+    {
+        std::lock_guard<std::mutex> lock(mtx);
+        int blogLevel;
+        switch (level) {
+        case LogLevel::Debug:
+            blogLevel = LOG_DEBUG;
+            break;
+        case LogLevel::Info:
+            blogLevel = LOG_INFO;
+            break;
+        case LogLevel::Warn:
+            blogLevel = LOG_WARNING;
+            break;
+        case LogLevel::Error:
+            blogLevel = LOG_ERROR;
+            break;
+        default:
+            fprintf(stderr, "[LOGGER FATAL] Unknown log level: %d\n", static_cast<int>(level));
+            return;
+        }
 
-		blog(blogLevel, "%.*s", static_cast<int>(message.length()), message.data());
-	}
+        constexpr size_t MAX_LOG_CHUNK_SIZE = 4000;
 
-	void logException(const std::exception &e, std::string_view context) const noexcept override
-	try {
-		error("{}: {}", context, e.what());
+        if (message.length() <= MAX_LOG_CHUNK_SIZE) {
+            blog(blogLevel, "%.*s", static_cast<int>(message.length()), message.data());
+        } else {
+            for (size_t i = 0; i < message.length(); i += MAX_LOG_CHUNK_SIZE) {
+                const auto chunk = message.substr(i, MAX_LOG_CHUNK_SIZE);
+                blog(blogLevel, "%.*s", static_cast<int>(chunk.length()), chunk.data());
+            }
+        }
+    }
 
-		backward::StackTrace st;
-		st.load_here(32);
+    void logException(const std::exception &e, std::string_view context) const noexcept override
+    try {
+        std::stringstream ss;
+        ss << context.data() << ": " << e.what() << "\n";
 
-		std::stringstream ss;
-		backward::Printer p;
-		p.print(st, ss);
-		log(LogLevel::Error, fmt::format("{}--- Stack Trace ---\n{}", getPrefix(), ss.str()));
-	} catch (const std::exception &log_ex) {
-		fprintf(stderr, "[LOGGER FATAL] Failed during exception logging: %s\n", log_ex.what());
-	} catch (...) {
-		fprintf(stderr, "[LOGGER FATAL] Unknown error during exception logging.\n");
-	}
+        backward::StackTrace st;
+        st.load_here(32);
+
+        backward::Printer p;
+        p.print(st, ss);
+
+        error("--- Stack Trace ---\n{}", ss.str());        
+    } catch (const std::exception &log_ex) {
+        fprintf(stderr, "[LOGGER FATAL] Failed during exception logging: %s\n", log_ex.what());
+    } catch (...) {
+        fprintf(stderr, "[LOGGER FATAL] Unknown error during exception logging.\n");
+    }
 
 protected:
-	std::string_view getPrefix() const noexcept override { return prefix; }
+    std::string_view getPrefix() const noexcept override { return prefix; }
 
 private:
-	const std::string prefix;
+    const std::string prefix;
+    mutable std::mutex mtx;
 };
 
 } // namespace BridgeUtils
